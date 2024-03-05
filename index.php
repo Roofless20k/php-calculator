b76af5f381127e57323d21a5226e93d8b507e5b2
IurySilva <celfabri123@gmail.com>
2024-03-05 15:47:10 -0500

    adds form submit check via post and collects all values from the associative array that is passed to PHP

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Calculator</title>
 </head>
+
 <body>
-    <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
+    <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 
         <input type="text" name="num1" placeholder="Number 1">
         <input type="text" name="num2" placeholder="Number 2">
@@ -18,5 +20,22 @@
         </select>
         <button type="submit" name="submit" value="submit">Calculate</button>
     </form>
+
+    <?php
+    // check if the form is submitted correctly
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // get the values from the form
+        //sanitize data to prevent security threats
+        $num1 = filter_input(INPUT_POST, 'num1', FILTER_SANITIZE_NUMBER_FLOAT);
+        $num2 = filter_input(INPUT_POST, 'num2', FILTER_SANITIZE_NUMBER_FLOAT);
+        $operator = htmlspecialchars($_POST['operator']);
+
+        
+    }
+
+    ?>
+
 </body>
+
 </html>
