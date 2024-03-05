0380eb38420b315d31e1b7b246fd65391587800b
IurySilva <celfabri123@gmail.com>
2024-03-05 15:08:40 -0500

    basic html form for a calculator that sends user data back to same page

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
     <title>PHP Calculator</title>
 </head>
 <body>
-    <?= "Hello World"; ?>
+    <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
+
+        <input type="text" name="num1" placeholder="Number 1">
+        <input type="text" name="num2" placeholder="Number 2">
+        <select name="operator" id="">
+            <option value="add">Add</option>
+            <option value="subtract">Subtract</option>
+            <option value="multiply">Multiply</option>
+            <option value="divide">Divide</option>
+        </select>
+        <button type="submit" name="submit" value="submit">Calculate</button>
+    </form>
 </body>
 </html>
